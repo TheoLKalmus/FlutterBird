8a005d2d06707eb31e0352d87d66dc299f17061b
TK1910 <theo.kalmus.tk@gmail.com>
2021-03-08 16:04:03 -0300

    Atualizacao de icone/nome da aplicacao e passaro

--- lib/bird.dart
+++ lib/bird.dart
@@ -4,8 +4,8 @@ class MyBird extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Container(
-      height: 60,
-      width: 60,
+      height: 90,
+      width: 90,
       child: Image.asset(
       'lib/images/flappy_ston_bird.png'
         ),
